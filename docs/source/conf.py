147b99efb59255a3cc326a5c0ba3d06ade8bccf7
dbdimitrov <daniel.boykov.dimitrov@gmail.com>
2025-05-27 11:21:57 +0200

    restructure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ html_theme_options = {
 }
 
 # static assets & your DataTables CSS/JS remain unchanged
-html_static_path = ['docs/_static']
+html_static_path = ['_static']
 html_css_files = [
     'https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css',
     "css/custom.css",
